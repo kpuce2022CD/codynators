b2e576ed5e79a2503e14ae20435489dc7e7514c6
khsexk <kohyunsuk98@gmail.com>
2022-02-17 22:37:53 +0900

    musicsheet

--- 02_MusicSheet/pdf2image/app.py
+++ 02_MusicSheet/pdf2image/app.py
@@ -1,8 +1,8 @@
 from pdf2image import convert_from_path
 
-file_name = "gooday.pdf"
+file_name = "gotrip.pdf"
 
-pages = convert_from_path("./" + file_name)
+pages = convert_from_path("./musicsheet/" + file_name)
 
 for i, page in enumerate(pages):
     page.save("./source/"+file_name+str(i)+".jpg", "JPEG")
