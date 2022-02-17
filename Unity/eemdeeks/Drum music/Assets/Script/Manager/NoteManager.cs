f0b683d7a9b146cf4ad78fa1a444389a76f2bb90
eemdeeks <prince9088@gmail.com>
2022-02-17 20:52:09 +0900

    update unity drum music
    test

---
+++ Unity/eemdeeks/Drum music/Assets/Script/Manager/NoteManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    public int bpm = 0;
+    double currentTime = 0d;
+
+    [SerializeField] Transform tfNoteAppear = null;
+    [SerializeField] GameObject goNote = null;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        
+        if(currentTime >= 60d / bpm)
+        {
+            GameObject t_note = Instantiate(goNote, tfNoteAppear.position, Quaternion.identity);
+            t_note.transform.SetParent(this.transform);
+            currentTime -= 60d / bpm;
+        }
+    }
+}
