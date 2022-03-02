7cebb2305a74debe2760333db916e4b377caeb79
khsexk <kohyunsuk98@gmail.com>
2022-03-02 15:04:32 +0900

    open-cv

--- 02_MusicSheet/pdf2image/app.py
+++ 02_MusicSheet/pdf2image/app.py
@@ -1,8 +1,15 @@
 from pdf2image import convert_from_path
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 file_name = "gotrip.pdf"
+
+thresh = 0
+maxValue = 255
 
 pages = convert_from_path("./musicsheet/" + file_name)
 
 for i, page in enumerate(pages):
+    image = cv2.imread('threshold.png')
     page.save("./source/"+file_name+str(i)+".jpg", "JPEG")
