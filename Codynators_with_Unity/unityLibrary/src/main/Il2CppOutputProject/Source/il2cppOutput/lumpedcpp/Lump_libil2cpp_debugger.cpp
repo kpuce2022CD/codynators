09779acbc03c801d5a37b40bf943e390aa7bf6b5
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-03-06 20:10:04 +0900

    update

--- Codynators_with_Unity/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\rldkv\AndroidStudioProjects\Codynators_with_Unity\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\debugger\il2cpp-stubs.cpp"
--- Codynators_with_Unity/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_gc.cpp
+++
@@ -1,7 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\rldkv\AndroidStudioProjects\Codynators_with_Unity\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\gc\BoehmGC.cpp"
-#include "C:\Users\rldkv\AndroidStudioProjects\Codynators_with_Unity\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\gc\GCHandle.cpp"
-#include "C:\Users\rldkv\AndroidStudioProjects\Codynators_with_Unity\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\gc\GarbageCollector.cpp"
-#include "C:\Users\rldkv\AndroidStudioProjects\Codynators_with_Unity\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\gc\NullGC.cpp"
-#include "C:\Users\rldkv\AndroidStudioProjects\Codynators_with_Unity\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\gc\WriteBarrier.cpp"
-#include "C:\Users\rldkv\AndroidStudioProjects\Codynators_with_Unity\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\gc\WriteBarrierValidation.cpp"
