57f37ed2d0f9a005c4f786f8048d2d42c2f30d21
khsexk <kohyunsuk98@gmail.com>
2022-03-06 16:53:34 +0900

    이진화

--- 02_MusicSheet/pdf2image/app.py
+++ 02_MusicSheet/pdf2image/app.py
@@ -5,11 +5,23 @@ import matplotlib.pyplot as plt
 
 file_name = "gotrip.pdf"
 
-thresh = 0
-maxValue = 255
 
 pages = convert_from_path("./musicsheet/" + file_name)
 
 for i, page in enumerate(pages):
-    image = cv2.imread('threshold.png')
-    page.save("./source/"+file_name+str(i)+".jpg", "JPEG")
+    page.save("./source/" + file_name + str(i) + ".jpg", "JPEG")
+    image = cv2.imread("./source/" + file_name + str(i) + ".jpg")
+
+    thresh = 0
+    maxValue = 255
+
+    th, dst = cv2.threshold(image, thresh, maxValue, cv2.THRESH_BINARY)
+
+    plt.figure(figsize=(14, 5))
+    plt.subplot(121), plt.imshow(image, cmap='gray'), plt.title('original image', fontsize=15)
+    plt.xticks([]), plt.yticks([])
+
+    plt.subplot(122), plt.imshow(dst, cmap='gray'), plt.title('filtered image', fontsize=15)
+    plt.xticks([]), plt.yticks([])
+    plt.show()
+
---
+++ 02_MusicSheet/pdf2image/functions.py
@@ -0,0 +1,7 @@
+import cv2
+
+
+def threshold(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+    return image
