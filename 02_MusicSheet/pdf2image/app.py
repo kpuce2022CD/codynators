eea44a27aa81e6a5acf4aad08df4c2417a86e7b9
khsexk <kohyunsuk98@gmail.com>
2022-03-06 17:33:39 +0900

    이진화

--- 02_MusicSheet/pdf2image/app.py
+++ 02_MusicSheet/pdf2image/app.py
@@ -2,26 +2,21 @@ from pdf2image import convert_from_path
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import functions as fs
+import os
 
 file_name = "gotrip.pdf"
-
 
 pages = convert_from_path("./musicsheet/" + file_name)
 
 for i, page in enumerate(pages):
-    page.save("./source/" + file_name + str(i) + ".jpg", "JPEG")
-    image = cv2.imread("./source/" + file_name + str(i) + ".jpg")
+    file = "./source/" + file_name + str(i) + ".jpg"
+    #page.save(file, "JPEG")
 
-    thresh = 0
-    maxValue = 255
+    image = cv2.imread(file)
+    image = fs.threshold(image)
 
-    th, dst = cv2.threshold(image, thresh, maxValue, cv2.THRESH_BINARY)
+    cv2.imshow('image', image)
+    cv2.imwrite("./source/" + "bnw" + str(i) + ".png", image)
 
-    plt.figure(figsize=(14, 5))
-    plt.subplot(121), plt.imshow(image, cmap='gray'), plt.title('original image', fontsize=15)
-    plt.xticks([]), plt.yticks([])
-
-    plt.subplot(122), plt.imshow(dst, cmap='gray'), plt.title('filtered image', fontsize=15)
-    plt.xticks([]), plt.yticks([])
-    plt.show()
 
