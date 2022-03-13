42c170bd92b78c4af278afb8d58ff062c5465500
khsexk <kohyunsuk98@gmail.com>
2022-03-14 00:44:25 +0900

    악보 내 객체 검출

--- 02_MusicSheet/pdf2image/app.py
+++ 02_MusicSheet/pdf2image/app.py
@@ -1,6 +1,7 @@
 from pdf2image import convert_from_path
 import cv2
 import preprocessing
+import objectProcessing
 import numpy as np
 import matplotlib.pyplot as plt
 import functions as fs
@@ -14,20 +15,19 @@ for i, page in enumerate(pages):
     file = "./source/" + file_name + str(i) + ".jpg"
     page.save(file, "JPEG")
 
-
-
-    #file2 = "./source/sample.png"
-
     image = cv2.imread(file)
 
     masked_image = preprocessing.rmNoise(image)
 
-    #noStaves_img, staves = preprocessing.rmStaves(masked_image)
+    noStaves_img, staves = preprocessing.rmStaves(masked_image)
 
-    #normal_MS, staves = preprocessing.musicsheetNormalization(noStaves_img, staves, 30)
+    normal_MS, staves = preprocessing.musicsheetNormalization(noStaves_img, staves, 150)
+    cv2.imwrite("./source/" + "정규화" + str(i) + ".png", normal_MS)
 
-    #cv2.imshow('image', normal_MS)
-    cv2.imwrite("./source/" + "test" + str(i) + ".png", masked_image)
+    # 4. 객체 검출 과정
+    findObject, objects = objectProcessing.object_detection(normal_MS, staves)
+
+    cv2.imwrite("./source/" + "find" + str(i) + ".png", findObject)
     print("exit")
 
 
