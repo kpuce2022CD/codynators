5644382506a4a7e7bf01bea6d581ad58e8c97daa
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-03-15 23:26:53 +0900

    TextInputLayout 사용
    이메일, 비번, 이름 임시

--- AndroidProject/Codrum/app/src/main/java/com/example/codrum/MainActivity.kt
+++ AndroidProject/Codrum/app/src/main/java/com/example/codrum/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.codrum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.codrum.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding : ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
+    private fun isVaildateEmail() : Boolean {
+        val value : String = binding.tilEmail.editText?.text.toString()
+        val pattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+
+        return if (value.isEmpty()) {
+            binding.tilEmail.error = "이메일을 입력해 주세요."
+            false
+        } else if (!value.matches(pattern.toRegex())){
+            binding.tilEmail.error = "이메일 형식을 맞추어주세요."
+            false
+        } else{
+            binding.tilEmail.error = null
+            binding.tilEmail.isErrorEnabled = false
+            true
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
 }
