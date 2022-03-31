6812402ca217da54702174347b356d7613622b3d
TAEYONG KIM <amico741@naver.com>
2022-03-31 21:54:14 +0900

    template match test

---
+++ templeteMatching/templete/templete.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+
+img_color = cv2.imread('img/original.jpg', cv2.IMREAD_COLOR)
+img_original = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
+
+img_template = cv2.imread('img/image.jpg', cv2.IMREAD_GRAYSCALE)
+
+# 이미지의 넓이와 높이를 가져온다
+original_h, original_w = img_original.shape[:2]
+template_h, template_w = img_template.shape[:2]
+
+best_position_y = 0
+best_position_x = 0
+best_position_sad = 100000
+
+#이미지의 모든좌표에서 SAD를 저장할 위치
+point = []
+
+# 원본 이미지 스캔
+for original_x in range(original_w - template_w):
+    for original_y in range(original_h - template_h):
+        SAD = 0
+
+        original_pixel = img_original[original_y:original_y + template_h, original_x:original_x + template_w].ravel()
+        template_pixel = img_template.ravel()
+
+        SAD = np.abs(np.subtract(original_pixel, template_pixel, dtype=np.float))
+        SAD = SAD.sum()
+
+        #최소 SAD 지점 찾기
+        if best_position_sad > SAD:
+            best_position_y = original_y
+            best_position_x = original_x
+            best_position_sad = SAD
+        point.append((original_x, original_y, SAD))
+
+
+for p in point:
+    if np.abs(p[2] - best_position_sad) < 100:
+        print(p[2])
+        cv2.rectangle(img_color, (p[0], p[1]), (p[0]*template_w, p[1]*template_h), (255, 0, 0), 3)
+
+cv2.imshow('result', img_color)
+cv2.waitKey(0)
+
+
+
