3c325e3dfca5f1d135a80476c6887245545b0813
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-04-22 20:05:51 +0900

    retrofit tmp

--- AndroidProject/Codrum/app/src/main/java/com/example/codrum/Fragment/UploadFragment.kt
+++ AndroidProject/Codrum/app/src/main/java/com/example/codrum/Fragment/UploadFragment.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,12 +14,18 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.example.codrum.Dialog.LoadingDialog
+import com.example.codrum.IO.RetrofitClass
+import com.example.codrum.IO.Song
 import com.example.codrum.databinding.FragmentUploadBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import org.jetbrains.anko.support.v4.toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class UploadFragment: Fragment() {
 
@@ -80,6 +87,24 @@ class UploadFragment: Fragment() {
             toast("업로드 완료")
             dialog.dismiss()
         }
+
+        RetrofitClass.retrofitService.putSong(userUID,binding.editSongName.text.toString()).enqueue(object : Callback<Song>{
+            override fun onResponse(call: Call<Song>, response: Response<Song>) {
+                when(response!!.code()){
+                    200 -> {
+                        Log.d("api","success")
+                    }
+                    405 -> {
+                        Log.d("api","method error")}
+                    500 -> {
+                        Log.d("api","server error")
+                    }
+                }
+            }
+            override fun onFailure(call: Call<Song>, t: Throwable) {
+            }
+        })
+
     }
 
 }
